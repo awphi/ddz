af7c4a0cf7752c1ef096c7bd3ec13f7645cfa7dd
awphi <26072111+awphi@users.noreply.github.com>
2025-04-29 18:56:16 +0800

    separate event bus for clarity

--- packages/lib/src/server.ts
+++ packages/lib/src/server.ts
@@ -1,41 +1,6 @@
+import { EventBus } from "./event-bus";
 import { GameState, Message } from "./types";
 import { createGame, isValidBid } from "./utils";
-
-class EventBus<T extends Record<string, (...args: any[]) => void>> {
-  private listeners: { [K in keyof T]?: T[K][] } = {};
-
-  on<K extends keyof T>(event: K, callback: T[K]) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
-    }
-    this.listeners[event]!.push(callback);
-  }
-
-  off<K extends keyof T>(event: K, callback: T[K]) {
-    if (this.listeners[event]) {
-      this.listeners[event] = this.listeners[event]!.filter(
-        (cb) => cb !== callback
-      );
-    }
-  }
-
-  once<K extends keyof T>(event: K, callback: T[K]) {
-    const wrappedCallback = ((...args) => {
-      callback(...args);
-      this.off(event, wrappedCallback);
-    }) as T[K];
-
-    this.on(event, wrappedCallback);
-  }
-
-  fire<K extends keyof T>(event: K, ...args: Parameters<T[K]>) {
-    if (this.listeners[event]) {
-      for (const callback of this.listeners[event]!) {
-        callback(...args);
-      }
-    }
-  }
-}
 
 /**
  * Dou Dizhu server class.
